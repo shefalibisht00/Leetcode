46606da1814c3a23157582e53d7f62eb4f827c50
Shefali Bisht <shefalibisht00@gmail.com>
2022-05-09 23:18:22 +0530

    Time: 970 ms (53.19%), Space: 0B (100.00%) - LeetHub

---
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+
+SELECT
+    a.Name AS 'Employee'
+FROM
+    Employee AS a,
+    Employee AS b
+WHERE
+    a.ManagerId = b.Id
+        AND a.Salary > b.Salary
+;
